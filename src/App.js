061cb7a020489a5bcf966539ba80d2315e0e7b79
Pavel Groshevskiy <pavel.groshevskiy@gmail.com>
2023-05-16 08:50:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,81 +3,42 @@ import "./App.css";
 import React from "react";
 import Tree from "react-d3-tree";
 import AddTreeModal from "./components/AddTreeModal";
+import { bfs, clickDelete } from "./utils/utils";
+
+const initialTree = {
+	name: "root",
+	attributes: {
+		surName: "surName",
+		suName: "surName",
+		srName: "surName",
+		Delete: "del",
+	},
+	children: [],
+};
 
 function App() {
 	const [node, setNode] = React.useState();
-	const [tree, setTree] = React.useState({
-		name: "root",
-		attributes: {
-			surName: "surName",
-			suName: "surName",
-			srName: "surName",
-			Delete: "del",
-		},
-		children: [],
-	});
+	const [tree, setTree] = React.useState(initialTree);
 
-	function clickDelete() {
-		const a = document.querySelectorAll("tspan");
-
-		a.forEach((el) => {
-			el.addEventListener("click", () => {
-				console.log(tree);
-
-				// console.log("click");
-			});
-		});
-	}
-	clickDelete();
-
-	function remove(id) {
-		tree.children.forEach((el) => console.log(el.data));
-	}
-	remove();
-	// console.log(node && node.data.__rd3t);
+	console.log(node && node.data.__rd3t);
+	console.log(node && node.children);
 
 	const closeModal = () => setNode();
 
 	const handleSubmit = (name, birthday, male, surName) => {
 		const newTree = bfs(node.data.name, tree, name, { birthday, male, surName });
 
-		if (newTree) setTree(newTree);
-	};
-
-	function bfs(name, tree, newNodeName, { surName, birthday, male }) {
-		const queue = [];
-
-		queue.unshift(tree);
-		while (queue.length > 0) {
-			const currNode = queue.pop();
-			console.log(currNode);
-
-			if (currNode.name === name) {
-				currNode.children.push({
-					name: newNodeName,
-					attributes: {
-						surName: surName,
-						birthday: birthday,
-						male: male,
-						Delete: "delete",
-					},
-					children: [],
-				});
-				return { ...tree };
-			}
-			for (let i = 0; i < currNode.children.length; i++) {
-				queue.unshift(currNode.children[i]);
-			}
+		if (newTree) {
+			setTree(newTree);
+			clickDelete();
 		}
-	}
-
-	function parent(data) {
-		console.log(data.parent);
-	}
+	};
 
 	return (
 		<Box h={"100vh"} w={"100%"}>
 			<Tree
+				data={tree}
+				collapsible={false}
 				nodeSize={{
 					x: "210",
 					y: "150",
@@ -87,10 +48,9 @@ function App() {
 					x: 200,
 					y: 200,
 				}}
-				data={tree}
 				onNodeClick={(data, event) => {
 					setNode(data);
-					console.log(data);
+					// console.log(data);
 					// parent(data);
 					// console.log(event.target && event.target.parentNode.getAttribute("id"));
 
---
+++ src/utils/utils.js
@@ -0,0 +1,59 @@
+function recursiveFunc(structure) {
+	if (Array.isArray(structure)) {
+		structure.filter();
+	} else {
+	}
+}
+
+//--------------------------
+
+export function clickDelete() {
+	const a = document.querySelectorAll("text.rd3t-label__attributes");
+	// console.log(a);
+	a.forEach((el) => {
+		console.log(el);
+		// el.lastChild.appendChild(but);
+		const but = `<button>ds</button>`;
+		el.lastChild.addEventListener("click", (e) => {
+			e.stopPropagation();
+			console.log("click");
+		});
+	});
+}
+
+//--------------------------
+
+//--------------------------
+
+export function bfs(name, tree, newNodeName, { surName, birthday, male }) {
+	const queue = [];
+
+	queue.unshift(tree);
+	while (queue.length > 0) {
+		const currNode = queue.pop();
+		console.log(currNode);
+
+		if (currNode.name === name) {
+			currNode.children.push({
+				name: newNodeName,
+				attributes: {
+					surName: surName,
+					birthday: birthday,
+					male: male,
+					Delete: "delete",
+				},
+				children: [],
+			});
+			return { ...tree };
+		}
+		for (let i = 0; i < currNode.children.length; i++) {
+			queue.unshift(currNode.children[i]);
+		}
+	}
+}
+
+//--------------------------
+
+function parent(data) {
+	console.log(data.parent);
+}
